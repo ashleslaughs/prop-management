18d66607765d6ef888f7accba3b87ec6dd71c8c3
ashley <lujanashlet@gmail.com>
2018-07-30 12:32:06 -0600

    authenticate user reducer

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,21 @@
+import {
+    AUTHENTICATE_USER
+} from '../actions/types';
+
+const INITIAL_STATE = {
+    authenticated: false,
+    user: [],
+}
+
+export default function(state = [], action) {
+    switch (action.type) {
+        case AUTHENTICATE_USER: 
+            const {user} = action.payload; 
+            return {
+                ...state, 
+                user
+            }
+
+        default: return state; 
+    }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,11 @@ import { combineReducers } from 'redux';
 
 import {reducers as form } from 'redux-form';
 
+import auth from './authReducer'
+
 const rootReducer = combineReducers({
-  form
+  form,
+  auth
 });
 
 export default rootReducer;
