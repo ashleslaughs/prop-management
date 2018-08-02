1bd980bf86ec4c26eaa09d9cc5900e860917276c
Ashley <lujanashlet@gmail.com>
2018-08-02 11:42:11 -0600

    styled newsletter latest and applied row gaps to the newsletter grid

--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -7,7 +7,7 @@ class NewsletterLatest extends Component {
                 <h1 className='newsletter-latest__title'>Title goes here </h1>
                 <img className='newsletter-latest__image' src='http://via.placeholder.com/960x258 ' />
                 <div className='newsletter-latest__body'>
-                    <p> Lorem ipsum </p>
+                    <p> Lorem ipsum. this is a bunch of random text because that is what he put is lorem ipsu has been the induryt's standard dummy text loool . he was in the industry ? </p>
                 </div>
             </div>
         )
