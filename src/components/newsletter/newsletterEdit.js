cce972a7256a70bf98ffbee6aecf6ed4c79cc8f8
Ashley <lujanashlet@gmail.com>
2018-08-07 14:24:40 -0600

    continuesd building newsletteredit feature

--- src/components/newsletter/newsletterEdit.js
+++ src/components/newsletter/newsletterEdit.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 import NewNewsletterForm from './newsletterNewForm.js';
 
@@ -13,15 +15,16 @@ class EditNewsletter extends Component {
     }
 
     componentDidMount() {
-        console.log(this.props.match.params.id);
+        this.props.fetchNewsletterWithId(this.props.match.params.id);
     }
 
     render() {
         return (
             <div className='new-newsletter'>
                 <NewNewsletterForm 
+                    newsletterToEdit={this.props.newsletterToEdit}
                     onCancel={() => this.onCancel()} 
-                    onSubmit={(event) => this.onSubmit(event)}
+                    onSubmit={event => this.onSubmit(event)}
                     title='Edit Newsletter'  
                 />
             </div>
@@ -29,4 +32,11 @@ class EditNewsletter extends Component {
     }
 }
 
-export default EditNewsletter; 
+function mapStateToProps(state) {
+    const { newsletterToEdit } = state.newsletters;
+    return {
+        newsletterToEdit
+    }
+}
+
+export default connect(null, actions)(EditNewsletter); 
